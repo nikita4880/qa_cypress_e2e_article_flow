915936c13cab49f04a41f2fa8a854a9c489e9739
niita4880 <niknikitich07@gmail.com>
2026-01-10 01:02:55 +0200

    Solutio

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,13 +1,60 @@
 describe('', () => {
   before(() => {
-
+    cy.visit('/');
   });
 
-  it('', () => {
+  it('should create article', () => {
+    const numberOfArticles = Math.floor(Math.random() * 1000);
+    cy.task('generateUser').then((user) => {
+      cy.login(user.email, user.username, user.password);
 
+      cy.visit('/');
+
+      // Перевірка: користувач залогінений
+      cy.contains(user.username.toLowerCase()).should('be.visible');
+
+      // Створення статті
+      cy.createArticle(`Cypress Test ${numberOfArticles}`, 'Test description', 'Test body');
+
+      // Перевірка: стаття існує
+      cy.visit('/');
+      cy.reload();
+      cy.get('.nav > :nth-child(2) > .link').click();
+
+      cy.contains('Cypress Test ' + numberOfArticles).should('be.visible');
+    });
   });
 
-  it('', () => {
+  it('should delete article', () => {
+    const numberOfArticles = Math.floor(Math.random() * 1000);
+    cy.task('generateUser').then((user) => {
+      cy.login(user.email, user.username, user.password);
 
+      cy.visit('/');
+
+      // Перевірка: користувач залогінений
+      cy.contains(user.username.toLowerCase()).should('be.visible');
+
+      // Створення статті
+      cy.createArticle(`Cypress Test ${numberOfArticles}`, 'Test description', 'Test body');
+
+      // Перевірка: стаття існує
+      cy.visit('/');
+      cy.reload();
+      cy.get('.nav > :nth-child(2) > .link').click();
+
+      cy.contains('Cypress Test ' + numberOfArticles).should('be.visible');
+
+      // Видалення статті
+      cy.get('.col-md-9 > :nth-child(2)').click();
+      // eslint-disable-next-line max-len
+      cy.get('.container > .article-meta > :nth-child(3) > .btn-outline-danger')
+        .click();
+
+      // Перевірка: стаття видалена
+      cy.visit('/');
+      cy.get('.nav > :nth-child(2) > .link').click();
+      cy.contains('Cypress Test ' + numberOfArticles).should('not.exist');
+    });
   });
 });
