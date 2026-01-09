76d4d5e80be798571b1d575df16f2f525afa46ab
niita4880 <niknikitich07@gmail.com>
2026-01-10 01:15:55 +0200

    correction

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,60 +1,46 @@
-describe('', () => {
+describe('Article Management', () => {
   before(() => {
     cy.visit('/');
   });
 
-  it('should create article', () => {
-    const numberOfArticles = Math.floor(Math.random() * 1000);
+  beforeEach(() => {
     cy.task('generateUser').then((user) => {
       cy.login(user.email, user.username, user.password);
-
       cy.visit('/');
-
       // Перевірка: користувач залогінений
       cy.contains(user.username.toLowerCase()).should('be.visible');
-
-      // Створення статті
-      cy.createArticle(`Cypress Test ${numberOfArticles}`, 'Test description', 'Test body');
-
-      // Перевірка: стаття існує
-      cy.visit('/');
-      cy.reload();
-      cy.get('.nav > :nth-child(2) > .link').click();
-
-      cy.contains('Cypress Test ' + numberOfArticles).should('be.visible');
     });
+  });
+
+  it('should create article', () => {
+    const numberOfArticles = Math.floor(Math.random() * 1000);
+    cy.createArticle(`Cypress Test ${numberOfArticles}`, 'Test description', 'Test body');
+
+    // Перевірка: стаття існує
+    cy.visit('/');
+    cy.reload();
+    cy.get('.nav > :nth-child(2) > .link').click();
+    cy.contains('Cypress Test ' + numberOfArticles).should('be.visible');
   });
 
   it('should delete article', () => {
     const numberOfArticles = Math.floor(Math.random() * 1000);
-    cy.task('generateUser').then((user) => {
-      cy.login(user.email, user.username, user.password);
+    cy.createArticle(`Cypress Test ${numberOfArticles}`, 'Test description', 'Test body');
 
-      cy.visit('/');
+    // Перевірка: стаття існує
+    cy.visit('/');
+    cy.reload();
+    cy.get('.nav > :nth-child(2) > .link').click();
+    cy.contains('Cypress Test ' + numberOfArticles).should('be.visible');
 
-      // Перевірка: користувач залогінений
-      cy.contains(user.username.toLowerCase()).should('be.visible');
+    // Видалення статті — шукаємо по заголовку, а не по позиції
+    cy.contains('a.preview-link', 'Cypress Test ' + numberOfArticles).click();
+    cy.get('.container > .article-meta >' +
+      ':nth-child(3) > .btn-outline-danger').click();
 
-      // Створення статті
-      cy.createArticle(`Cypress Test ${numberOfArticles}`, 'Test description', 'Test body');
-
-      // Перевірка: стаття існує
-      cy.visit('/');
-      cy.reload();
-      cy.get('.nav > :nth-child(2) > .link').click();
-
-      cy.contains('Cypress Test ' + numberOfArticles).should('be.visible');
-
-      // Видалення статті
-      cy.get('.col-md-9 > :nth-child(2)').click();
-      // eslint-disable-next-line max-len
-      cy.get('.container > .article-meta > :nth-child(3) > .btn-outline-danger')
-        .click();
-
-      // Перевірка: стаття видалена
-      cy.visit('/');
-      cy.get('.nav > :nth-child(2) > .link').click();
-      cy.contains('Cypress Test ' + numberOfArticles).should('not.exist');
-    });
+    // Перевірка: стаття видалена
+    cy.visit('/');
+    cy.get('.nav > :nth-child(2) > .link').click();
+    cy.contains('Cypress Test ' + numberOfArticles).should('not.exist');
   });
 });
